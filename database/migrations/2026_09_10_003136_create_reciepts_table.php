6f9251bcb055b9b33a1484c8c57dde0a6949722f
fayrouzSameh <fayroz.sameh@gmail.com>
2026-09-10 05:06:14 +0300

    upload database migrations

---
+++ database/migrations/2026_09_10_003136_create_reciepts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reciepts', function (Blueprint $table) {
+            $table->id('reciept_id');
+            $table->foreignId('transaction_is')
+                  ->constrained('transactions', 'transaction_id')
+                  ->onDelete('cascade');
+            $table->decimal('amount_paid', 7, 2);
+            $table->date('date');
+            $table->string('img')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reciepts');
+    }
+};
